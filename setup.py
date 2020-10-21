b76438fce055c3f6647c1fdaef2faef721f96ef6
Your Name <daniel@kryptonunite.com>
2020-10-21 15:48:23 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup(
     name='oze_dataset',
-    version='0.0.1',
+    version='0.0.2',
     description='Downloads oze dataset and creates npz file.',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
